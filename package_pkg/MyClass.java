6cd71a41514f3c354431913b27b2a7aade500e69
Sakshi Sirgan <sakshisirgan03@gmail.com>
2025-08-13 21:02:20 +0530

    25 July 2025

---
+++ package_pkg/MyClass.java
@@ -0,0 +1,9 @@
+package package_pkg;
+
+import package_pkg.MyClass;
+
+public class MyClass {
+	public void display() {
+	      System.out.println("Hello from MyClass in mypackage!");
+	}
+}
---
+++ package_pkg/Test.java
@@ -0,0 +1,10 @@
+package package_pkg;
+
+public class Test { 
+	public static void main(String[] args) {
+
+    MyClass obj = new MyClass();
+    obj.display();
+	}
+
+}
