d7370557369ffd7491e225cb8af4fa1493b1db22
Sakshi Sirgan <sakshisirgan03@gmail.com>
2025-08-13 20:58:45 +0530

    21-22 July 2025 Java Fundamentals

---
+++ DefaultValue.java
@@ -0,0 +1,26 @@
+package Variables;
+
+class DefaultValue{
+	public static void main(String...args){
+		//A variable is declared
+		byte b = 2;
+		short s =2;
+		int x =10;
+		long k =10000;
+		float f = 2.5f;
+		double d = 3.567;
+		boolean val = true;
+		char c = 'A';
+		
+		System.out.println("Default byte variable value: "+b);
+		System.out.println("Default short variable value: "+s);
+		System.out.println("Default int variable value: "+x);
+		System.out.println("Default long variable value: "+k);
+		System.out.println("Default float variable value: "+f);
+		System.out.println("Default double variable value: "+d);
+		System.out.println("Default boolean variable value: "+val);
+		System.out.println("Default char variable value: "+c);
+		
+		
+	}
+}
